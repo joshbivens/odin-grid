d67d37fa3da63c239ccede54c3be14e19ca6df67
Josh Bivens <hotbioform@gmail.com>
2016-02-08 20:32:48 -0700

    Fix mouseenter on new grid
    Think we're pretty much finished. Now all that's left is refactor.

--- script.js
+++ script.js
@@ -1,38 +1,37 @@
 $(document).ready(function() {
 
   var clicked = false;
+  var initialColor = "#" + Math.floor(Math.random()*16777215).toString(16);
 
-  newGrid(7);
+  newGrid(7, initialColor);
 
 	$("#clear").on("click", function() {
 		$(".grid div").css("background", "none");
 	});
 
-	$(".grid div").on("mouseenter", function() {
-		$(this).css("background-color", "tomato");
-	});
-
   $("#restart").on("click", function() {
+    var color = "#" + Math.floor(Math.random()*16777215).toString(16);
     $(".grid").empty();
-    var size = prompt("Enter the size of the new grid: ");
-    newGrid(size);
+    var size = prompt("Enter the size of the new grid");
+    newGrid(size, color);
   });
 
   $("#random").on("click", function() {
     $(".grid div").on("mouseenter", function() {
-      $(this).css("background-color", (clicked ? "#" + Math.floor(Math.random()*16777215).toString(16) : "salmon"));
+      $(this).css("background-color", (clicked ? "#" + Math.floor(Math.random()*16777215).toString(16) : color));
     });
     clicked = !clicked;
   });
-});
 
-
-function newGrid(size) {
-  var width = ($(".grid").width() / size) - 4;
-
-  for (var j = 0, y = size * size; j < y; j++) {
-    $(".grid").append("<div></div>");
+  function newGrid(size, color) {
+    var width = ($(".grid").width() / size) - 4;
+    for (var j = 0, y = size * size; j < y; j++) {
+      $(".grid").append("<div></div>");
+    }
+    $(".grid div").css({"width": width, "height": width});
+    $(".grid div").on("mouseenter", function() {
+  		$(this).css("background-color", color);
+  	});
   }
 
-  $(".grid div").css({"width": width, "height": width});
-}
+});
